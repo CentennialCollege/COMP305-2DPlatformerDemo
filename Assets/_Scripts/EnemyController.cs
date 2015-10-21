9629c050a0495d356e7df221db7b9107c1aa141b
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-10-21 12:23:25 -0400

    added enemy prefab

--- Assets/_Scripts/EnemyController.cs
+++ Assets/_Scripts/EnemyController.cs
@@ -3,14 +3,17 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour {
 	// PUBLIC INSTANCE VARIABLES
-	public float speed = 50f;
+	public float speed = 100f;
+	public Transform sightStart;
+	public Transform sightEnd;
 
 	// PRIVATE INSTANCE VARIABLES
 	private Rigidbody2D _rigidbody2D;
 	private Transform _transform;
 	private Animator _animator;
 
-	private bool _isGrounded = true;
+	private bool _isGrounded = false;
+	private bool _isGroundAhead = true;
 
 	// Use this for initialization
 	void Start () {
@@ -24,7 +27,15 @@ public class EnemyController : MonoBehaviour {
 		// check if enemy is grounded
 		if (this._isGrounded) {
 			this._animator.SetInteger("AnimState", 1);
-			this._rigidbody2D.velocity = new Vector2(this.speed, 0f);
+			this._rigidbody2D.velocity = new Vector2 (this._transform.localScale.x, 0) * this.speed;
+
+			this._isGroundAhead = Physics2D.Linecast(this.sightStart.position, this.sightEnd.position,1 << LayerMask.NameToLayer ("Solid"));
+			Debug.DrawLine(this.sightStart.position, this.sightEnd.position);
+
+			if(this._isGroundAhead == false) {
+				this._flip ();
+			}
+
 		} else {
 			this._animator.SetInteger("AnimState", 0);
 		}
@@ -41,4 +52,14 @@ public class EnemyController : MonoBehaviour {
 			this._isGrounded =  false;
 		}
 	}
+
+	// PRIVATE METHODS
+	private void _flip() {
+		if (this._transform.localScale.x == 1) {
+			this._transform.localScale = new Vector3(-1f, 1f, 1f); 
+		} else {
+			this._transform.localScale = new Vector3(1f, 1f, 1f); // reset to normal scale
+		}
+	}
+
 }
